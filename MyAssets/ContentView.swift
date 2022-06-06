f3b0bdedaf10ede6cb916de64dfca0d9cbd141b9
Isaac Kim <myong328@gmail.com>
2022-06-06 10:18:01 -0700

    Initial Commit

---
+++ MyAssets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAssets
+//
+//  Created by Isaac Kim on 6/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by Isaac Kim on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
